5779350c2d0626ab83bf95f18a53630adad4c6c4
muhammadhussainjs <hussain356q@gmail.com>
2024-07-27 16:09:00 +0300

    test

--- FINAL HACKATHON/src/Screens/Login/Login.jsx
+++ FINAL HACKATHON/src/Screens/Login/Login.jsx
@@ -1,155 +1,9 @@
-// import React, { useRef } from 'react'
-// import { useNavigate } from 'react-router-dom';
-// import axios from 'axios'
-
-
-
-// const Login = () => {
-  
-//   const navigate = useNavigate()
-
-//   const emailref = useRef(null)
-//   const passwordref = useRef(null)
-
-//   const Getobj = async (e) =>{
-    
-//     e.preventDefault()
-
-//     const email = emailref.current.value
-//     const password = passwordref.current.value
-//     console.log({ email , password});
-
-//     try{
-//     const Data = {
-//       email : email,
-//       password : password,
-//     }
-//     console.log(Data);
-
-
-  
-//   const response = await axios.post('http://localhost:3001/users/login' , Data)
-//   console.log(response.data);
-//   if(response.data.message === 'User not found' ){
-//       alert('please check your email')
-//       return
-//   }
-//   if(response.data.message === 'Invalid Password' ){
-//       alert('please check your password')
-//       return
-
-//   }
-//   navigate('/')
-
-// } catch (error) {
-//   console.error('Error uploading file or signing up user: ', error);
-//   }}
-
-
-//   return (
-//     <>
-//       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-//         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-//           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-//             Sign in to your account
-//           </h2>
-//         </div>
-
-//         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-//           <form className="space-y-6" action="#" method="POST">
-//             <div>
-//               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-//                 Email address
-//               </label>
-//               <div className="mt-2">
-//                 <input
-//                   id="email"
-//                   name="email"
-//                   ref={emailref}
-//                   type="email"
-//                   autoComplete="email"
-//                   required
-//                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-//                 />
-//               </div>
-//             </div>
-
-//             <div>
-//               <div className="flex items-center justify-between">
-//                 <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-//                   Password
-//                 </label>
-//                 <div className="text-sm">
-//                   <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-//                     Forgot password?
-//                   </a>
-//                 </div>
-//               </div>
-//               <div className="mt-2">
-//                 <input
-//                   id="password"
-//                   name="password"
-//                   type="password"
-//                   ref={passwordref}
-//                   autoComplete="current-password"
-//                   required
-//                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-//                 />
-//               </div>
-//             </div>
-
-//             <div>
-//               <button
-//                 type="submit"
-//                 onClick={Getobj}
-//                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-//               >
-//                 Sign in
-//               </button>
-//             </div>
-//           </form>
-
-//           <p className="mt-10 text-center text-sm text-gray-500" onClick={()=>{navigate('/register')}}>
-//             Not a member?{' '}
-//             <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-//               Sign up
-//             </a>
-//           </p>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Login;
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 import React, { useRef } from 'react'
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios'
+import image from '../../assets/smitimage.png'
 
 const Login = () => {
   
@@ -172,7 +26,7 @@ const Login = () => {
       }
       console.log(Data);
 
-      const response = await axios.post('http://localhost:3001/users/login', Data)
+      const response = await axios.post('https://backened-with-mongodb-final-hackathon.vercel.app/login', Data)
       console.log(response.data);
 
       if (response.data.message === 'User not found!') {
@@ -194,18 +48,24 @@ const Login = () => {
     <>
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+
+          <img className='w-24 mx-auto ' src={image} alt="image"/>
+
+          <h2 className="mt-1 text-[#0075b7] text-center text-1.5xl font-semibold  leading-9 tracking-tight ">
+            Task Submission Portal
+          </h2>
+          <h2 className="mt-1 text-center text-[#73c54d] text-3xl font-semi-bold leading-9 tracking-tight">
+            Sign in
           </h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" onSubmit={handleSubmit}>
             <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
+              <label htmlFor="email" className="block text-blue-500 text-sm font-medium leading-6">
+                EMAIL
               </label>
-              <div className="mt-2">
+              <div>
                 <input
                   id="email"
                   name="email"
@@ -213,21 +73,21 @@ const Login = () => {
                   type="email"
                   autoComplete="email"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full outline-none rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Password
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-blue-500">
+                  PASSWORD
                 </label>
-                <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                {/* <div className="text-sm">
+                  <a href="#" className="font-semibold text-blue-500 hover:text-indigo-500">
                     Forgot password?
                   </a>
-                </div>
+                </div> */}
               </div>
               <div className="mt-2">
                 <input
@@ -237,7 +97,7 @@ const Login = () => {
                   ref={passwordref}
                   autoComplete="current-password"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full outline-none rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -245,16 +105,16 @@ const Login = () => {
             <div>
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="flex w-full justify-center rounded-md bg-blue-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign in
               </button>
             </div>
           </form>
 
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{' '}
-            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500" onClick={() => { navigate('/register'); }}>
+          <p className="mt-3 text-center text-sm text-gray-500">
+            Dont have n account?{' '}
+            <a href="#" className="font-semibold leading-6 text-blue-500 hover:text-indigo-500" onClick={() => { navigate('/register'); }}>
               Sign up
             </a>
           </p>
--- FINAL HACKATHON/src/Screens/Register/Register.jsx
+++ FINAL HACKATHON/src/Screens/Register/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import image from '../../assets/smitimage.png'
 
 const Register = () => {
 
@@ -26,7 +27,7 @@ const Register = () => {
       fullName : fullName,
       email : email,
       password : password,
-      confirmpassword : confirmpassword
+      // confirmpassword : confirmpassword
     }
     console.log(Data);
 if(password !== confirmpassword){
@@ -35,7 +36,7 @@ if(password !== confirmpassword){
 }
 
   
-  const response = await axios.post('http://localhost:3001/users/register' , Data)
+  const response = await axios.post('https://backened-with-mongodb-final-hackathon.vercel.app/register' , Data)
   console.log(response.data);
   if(response.data === 'please enter correct email' ){
       alert('please check it your email and password')
@@ -46,41 +47,87 @@ if(password !== confirmpassword){
 
 } catch (error) {
   console.error('Error uploading file or signing up user: ', error);
-  }}
+  }
+}
 
 
   
-  return (
+   return (
     <>
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-3 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign up to your account
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+
+          <img className='w-24 mx-auto ' src={image} alt="image"/>
+
+          <h2 className="mt-1 text-[#0075b7] text-center text-1.5xl font-semibold  leading-9 tracking-tight ">
+            Task Submission Portal
+          </h2>
+          <h2 className="mt-1 text-center text-[#73c54d] text-3xl font-semi-bold leading-9 tracking-tight">
+            Sign Up
           </h2>
         </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className="mt-3 sm:mx-auto  sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST" >
-          <div>
-              <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
-                Full Name
+            <div className='flex gap-7'>
+
+          <div className='mt-2'>
+              <label htmlFor="name" className="block text-blue-500 text-sm font-medium leading-6 ">
+                NAME
               </label>
-              <div className="mt-2">
+              
+              <div>
                 <input
                   id="name"
-                  name="email"
+                  name="name"
                   type="name"
                   required
                   ref={nameref}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full outline-none rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
+          {/* <div>
+              <label htmlFor="name" className="block text-blue-500 py-1 text-sm font-medium leading-6 ">
+                FATHER NAME
               </label>
-              <div className="mt-2">
+              
+              <div>
+                <input
+                  id="Father Name"
+                  name="name"
+                  type="name"
+                  required
+                  ref={nameref}
+                  className="block w-full outline-none rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+              </div>
+            </div> */}
+                  </div>
+                  <div className='flex gap-7'>
+
+            {/* <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-blue-500">
+                CNIC
+              </label>
+              <div className="">
+                <input
+                  id="email"
+                  name="email"
+                  type="name"
+                  autoComplete="email"
+                  required
+                  ref={emailref}
+                  className="block w-full outline-none rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div> */}
+
+             <div>
+              <label htmlFor="email" className="block text-blue-500 text-sm font-medium leading-6">
+                EMAIL
+              </label>
+              <div className="">
                 <input
                   id="email"
                   name="email"
@@ -88,18 +135,34 @@ if(password !== confirmpassword){
                   autoComplete="email"
                   required
                   ref={emailref}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full outline-none rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
-            </div>
-
+            </div> 
+</div> 
+  {/* <div className="flex gap-7">
+              <div className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                <select className="w-full">
+                  <option value="WEB AND APP">WEB AND APP</option>
+                  <option value="AI AND CHATBOT">AI AND CHATBOT</option>
+                  <option value="GRAPHIC DESIGNING">GRAPHIC DESIGNING</option>
+                </select>
+              </div>
+              <div className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                <select className="w-full">
+                  <option value="SIR KASHIF">SIR KASHIF</option>
+                  <option value="SIR ABDULLAH">SIR ABDULLAH</option>
+                  <option value="SIR GHOUS">SIR GHOUS</option>
+                </select>
+              </div>
+</div>   */}
             <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Password
+              <div className="flex items-center gap-36">
+                <label htmlFor="password" className="block  text-sm font-medium leading-6 text-blue-500">
+                  PASSWORD
                 </label>
                 <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                  <a href="#" className="font-semibold text-blue-500 hover:text-indigo-500">
                     Forgot password?
                   </a>
                 </div>
@@ -112,14 +175,14 @@ if(password !== confirmpassword){
                   autoComplete="current-password"
                   required
                   ref={passwordref}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block outline-none w-[90%] rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
              <div>
                <div className="flex items-center justify-between"> 
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Confirm Password
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-blue-500">
+                  CONFIRM PASSWORD
                 </label>
                 
               </div>
@@ -131,7 +194,7 @@ if(password !== confirmpassword){
                   autoComplete="current-password"
                   required
                   ref={confirmpasswordref}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-[90%] outline-none rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div> 
@@ -140,16 +203,16 @@ if(password !== confirmpassword){
               <button
                 type="submit"
                 onClick={Getobj}
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="flex w-[90%] justify-center rounded-md bg-blue-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign up
               </button>
             </div>
           </form>
 
-          <p className="mt-10 text-center text-sm text-gray-500">
+          <p className="mt-3 text-center text-sm text-gray-500">
             already a user?{' '}
-            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500" onClick={()=>{navigate('/login')}}>
+            <a href="#" className="font-semibold leading-6 text-blue-500 hover:text-indigo-500" onClick={()=>{navigate('/login')}}>
               Sign in
             </a>
           </p>
