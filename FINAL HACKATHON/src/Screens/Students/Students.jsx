608913f1da797d0a3aaee226a83e4e4fc360cd54
muhammadhussainjs <hussain356q@gmail.com>
2024-08-12 02:40:46 +0300

    push

--- FINAL HACKATHON/src/Screens/Students/Students.jsx
+++ FINAL HACKATHON/src/Screens/Students/Students.jsx
@@ -13,6 +13,8 @@ const Students = () => {
         const fetchAssignments = async () => {
             try {
                 const response = await axios.get(`https://backened-with-mongodb-final-hackathon.vercel.app/assignments/students/${uniqueIdentifier}`);
+                console.log(response.data);
+                
                 setAssignments(response.data.data);
             } catch (error) {
                 console.error('Error fetching assignments:', error);
