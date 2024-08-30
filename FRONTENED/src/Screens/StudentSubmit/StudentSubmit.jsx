7a23e8c942dfbee13082f5020662ef8029058d65
muhammadhussainjs <hussain356q@gmail.com>
2024-08-30 16:02:48 +0300

    test

--- FRONTENED/src/Screens/StudentSubmit/StudentSubmit.jsx
+++ FRONTENED/src/Screens/StudentSubmit/StudentSubmit.jsx
@@ -103,7 +103,7 @@ try {
           type="text"
           autoComplete="email"
           required
-          className="block w-full outline-none rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          className="block w-full outline-none rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
         />
       </div>
     </div>
